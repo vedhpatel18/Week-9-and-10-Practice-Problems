d0bc1b8905df1729a347cfc7d6f6cccef26f0136
vedhpatel18 <halpanivedh189@gmail.com>
2026-05-06 17:07:29 +0530

    Level 3 Added

---
+++ FirstNonRepeatingChar.java
@@ -0,0 +1,42 @@
+/* 
+@author Vedh
+*/
+import java.util.Scanner;
+
+public class FirstNonRepeatingChar {
+
+    public static char findFirstNonRepeating(String text) {
+        int[] freq = new int[256]; // ASCII size
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            freq[ch]++;
+        }
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (freq[ch] == 1) {
+                return ch;
+            }
+        }
+
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        char result = findFirstNonRepeating(input);
+
+        if (result != '\0') {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("No non-repeating character found.");
+        }
+
+        sc.close();
+    }
+}
